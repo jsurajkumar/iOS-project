e84dc0fc742e57a432389822604f79266f0450f9
jsurajkumar <62197637+jsurajkumar@users.noreply.github.com>
2022-11-04 12:23:30 +0530

    Add files via upload

---
+++ Suraj Project/Selina/Selina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Selina
+//
+//  Created by Student on 04/11/22.
+//  Copyright © 2022 LPUStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
